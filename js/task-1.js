77ea3f451b1901b332f8fe72f2ef928c25faf8c7
Виталий Еленчук <135379297+vitalii1408@users.noreply.github.com>
2026-06-14 11:50:30 +0300

    task-1

---
+++ js/task-1.js
@@ -0,0 +1,9 @@
+function isEnoughCapacity(products, containerSize) {
+  const total = Object.values(products).reduce((acc, amount) => acc + amount, 0);
+  return total <= containerSize;
+}
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); 
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));  
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); 
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); 
